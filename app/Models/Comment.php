47cae8c28ab58b96978720a9f6bd2f27110a803f
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-02-23 22:28:55 +0100

    feat: :fire: Completado del modelo
    Completado del modelo Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,10 +1,45 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'text',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     * 
+     * @var list<string>
+     */
+
+    protected $hidden = [
+        'user_id',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'user_id' => 'integer',
+            'text' => 'string',
+        ];
+    }
+
 }
