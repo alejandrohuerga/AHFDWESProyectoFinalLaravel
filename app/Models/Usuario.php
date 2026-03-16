bc5c6d715a868be978bed830a5ecb81eb25aabbf
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-03-16 15:56:04 +0100

    ADD : Extends de Usuario Modelo

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,18 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Clase Usuario (Representa un Usuario de la Base de Datos).
  * 
+ * Extiende de la clase Authenticatable lo cual nos permite decirle a Laravel
+ * que esta clase puede iniciar session , registrarse ...
  * 
  * @author Alejandro De la huerga.
  * @since 16/03/2026
  * @version 1.0.0
  */
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
     //
 }
